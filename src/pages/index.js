a085e5585199fd1cb23e6ca1c0f7e83f27758106
Zhannurkhan <zhannurkhan04@gmail.com>
2024-05-29 22:32:05 +0500

    fixed bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-  <div className={`min-h-screen flex items-center justify-center bg-[#F3F4F6] ${inter.className}`}>
+  <div className={`min-h-screen flex items-center justify-center bg-[#F3F4F6] text-black ${inter.className}`}>
     <LoginForm/>
   </div>
   );
